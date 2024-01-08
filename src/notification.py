93b953efc9e42262e44b91a0a0f321fd8a9d8386
K5HV <115079501+kshvsec@users.noreply.github.com>
2024-01-09 00:59:15 +0530

    Update notification.py

--- src/notification.py
+++ src/notification.py
@@ -1,16 +1,24 @@
 import ctypes
 import pygame
+from plyer import notification
 
-def notification(title, message, file_path):
+def popup(title, message, file_path):
     pygame.mixer.init()
     pygame.mixer.music.load(file_path)
     pygame.mixer.music.play()
     ctypes.windll.user32.MessageBoxW(0, message, title, 1)
     pygame.mixer.quit()
+    notification.notify(
+        title="Reminder Completed",
+        message=message,
+        app_name="Koala Reminder",
+        app_icon=None,  
+        timeout=10
+    )
 
 #series:
 # innit / load
 # play sound 
 # display notifis
 # end
-# notification('Reminder', 'Note Value', 'notification.mp3')
+# popup('Reminder', 'Note Value', 'src\\notification.mp3')
